d3c79c9bbb1df48ff379ec86d7b13a622a3a3ea2
wangminan <wangminan0811@hotmail.com>
2025-02-20 16:37:54 +0800

    【FE】首次提交

---
+++ src/main/java/edu/npu/FlinkAgentApplication.java
@@ -0,0 +1,13 @@
+package edu.npu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlinkAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlinkAgentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/npu/FlinkAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.npu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlinkAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
